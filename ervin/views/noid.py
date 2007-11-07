1344e64cdefd0f3cf7039504c6358baa1e1205aa
Erik Hetzner <ehetzner@gmail.com>
2007-11-06 23:28:42 -0800

    clean up noid.py

--- ervin/views/noid.py
+++ ervin/views/noid.py
@@ -17,26 +17,20 @@ from django.template import Context, loader
 from ervin.models import *
 from django.http import HttpResponse,HttpResponseNotFound
 from django.core.exceptions import ObjectDoesNotExist
+from ervin.views.generic import find_one
 import ervin.views.generic
 import ervin.views.person
 import ervin.views.work
 import ervin.views.expression
 
+views = { Person : ervin.views.person.detail,	
+          Work : ervin.views.work.detail,
+          Expression : ervin.views.expression.detail }
+
 def by_noid(request,*args,**kwargs):
     n = kwargs['noid']
-    def try_view(klass, view_func):
-        try:
-            o = klass.objects.get(noid=n)
-            if o != None:
-                return view_func(o,request,*args,**kwargs)
-            else:
-                return None
-        except ObjectDoesNotExist:
-            return None
-    for pair in ((Person, ervin.views.person.detail),
-                 (Work, ervin.views.work.detail),
-                 (Expression, ervin.views.expression.detail)):
-        retval = try_view(pair[0], pair[1])
-        if retval != None:
-            return retval
-    return HttpResponseNotFound('Not found')
+    o = find_one(Person, Work, Expression, noid=n)
+    if views.has_key(o.__class__):
+        return views[o.__class__] (o, request, *args, **kwargs)
+    else:
+        return HttpResponseNotFound('Not found')
