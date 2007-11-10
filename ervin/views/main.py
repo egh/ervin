5f7ec2bbf35e04f2bdf3befb105a8008cce7d5fb
Erik Hetzner <ehetzner@gmail.com>
2007-11-09 21:32:44 -0800

    add main view

---
+++ ervin/views/main.py
@@ -0,0 +1,24 @@
+#Copyright (C) 2007 Erik Hetzner
+
+#This file is part of Ervin.  Ervin is free software: you can
+#redistribute it and/or modify it under the terms of the GNU General
+#Public License as published by the Free Software Foundation, either
+#version 3 of the License, or (at your option) any later version.
+
+#Ervin is distributed in the hope that it will be useful, but WITHOUT
+#ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Ervin.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.template import Context, loader
+from ervin.models import *
+from ervin.views.generic import *
+from django.http import HttpResponse
+
+def home(request, *args, **kwargs):
+    t = loader.get_template('home.html')
+    c = Context({})
+    return HttpResponse(t.render(c))
