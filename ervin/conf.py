a2668b1cc34c780ce687126c13c47c5328a180a4
Erik Hetzner <ehetzner@gmail.com>
2009-04-02 20:11:47 -0700

    fix settings const names

--- ervin/conf.py
+++ ervin/conf.py
@@ -2,8 +2,8 @@ from django.conf import settings
 
 FEED_AUTHOR = getattr(settings, "ERVIN_FEED_AUTHOR", "John Doe")
 FEED_EMAIL = getattr(settings, "ERVIN_FEED_EMAIL", "john.doe@example.org")
-NEWS_FEED_ID = getattr(settings, "NEWS__FEED_ID", "BROKEN FEED ID")
-NEWS_FEED_TITLE = getattr(settings, "NEWS_FEED_TITLE", "News")
+NEWS_FEED_ID = getattr(settings, "ERVIN_NEWS_FEED_ID", "BROKEN FEED ID")
+NEWS_FEED_TITLE = getattr(settings, "ERVIN_NEWS_FEED_TITLE", "News")
 RECENT_DOCUMENTS_FEED_ID = getattr(settings, "ERVIN_RECENT_DOCUMENTS_FEED_ID", "BROKEN FEED ID")
 RECENT_DOCUMENTS_FEED_TITLE = getattr(settings, "ERVIN_RECENT_DOCUMENTS_FEED_TITLE", "Recent documents")
 RECENT_PUBLICATIONS_FEED_ID = getattr(settings, "ERVIN_RECENT_PUBLICATIONS_FEED_ID", "BROKEN FEED ID")
